ef0767975df4587818b6bb9f210cb484b33b6f80
KSY <ggomaDVLP@gmail.com>
2023-01-02 17:47:52 +0900

    KSY 230102

--- InventoryFriends/src/Main.java
+++ InventoryFriends/src/Main.java
@@ -1,17 +1,19 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.plaf.*;
 
 public class Main extends JFrame{
-    JPanel jpLU, jpLD, jpRU, jpRD;
+    JPanel jpMain, jpLU, jpLD, jpRU, jpRD, jpBottom;
     JMenuBar jmbMenuBar;
     JMenu jmFileMenu;
-    JButton btnSignOut, btnInventoryManagement, btnOrderConsolidation;
+    JButton btnSignOut, btnInventoryManagement, btnOrderConsolidation, btnAlarm, btnIMopt1, btnIMopt2;
     JSplitPane jspCenter, jspLeft, jspRight;
-    JLabel jlUserName, jlIMopt1, jlIMopt2;
+    JLabel jlUserName, jlCalendar;
     JTabbedPane jtpMainTab, jtpMainTab2;
-    ImageIcon imgIM1, imgIM2, imgOC1, imgOC2, imgSO1, imgSO2;
+    ImageIcon imgIM1, imgIM2, imgOC1, imgOC2, imgSO1, imgSO2, imgAlarm1, imgAlarm2, imgCal,
+            imgIMopt1_1, imgIMopt1_2, imgIMopt2_1, imgIMopt2_2;
     MenuAction menuAct;
     Font font1, font2;
     private int sizeWidth = 1280;
@@ -47,8 +49,8 @@ public class Main extends JFrame{
                 jmFileMenu.addSeparator(); // 숨기기와 닫기 사이에 구분선 추가
         }
 
-        imgSO1 = new ImageIcon("./img/img_Button5.jpg");
-        imgSO2 = new ImageIcon("./img/img_Button6.jpg");
+        imgSO1 = new ImageIcon("./img/img_SO1.jpg");
+        imgSO2 = new ImageIcon("./img/img_SO2.jpg");
 
         jlUserName = new JLabel("UserNameTest");
         jlUserName.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 20));
@@ -82,25 +84,35 @@ public class Main extends JFrame{
     }
     private void createPanel(){
         // 폰트 설정
-        font1 = new Font("돋움", Font.BOLD, 20);   // 왼쪽 하위메뉴 라벨 폰트
+//        font1 = new Font("돋움", Font.BOLD, 20);   // 왼쪽 하위메뉴 라벨 폰트
         font2 = new Font("SansSerif", Font.BOLD, 14);   // 탭 타이틀 폰트
 
         jtpMainTab = new JTabbedPane();
         jtpMainTab2 = new JTabbedPane();
 
+        jpMain = new JPanel();
         jpLU = new JPanel();
         jpLD = new JPanel();
         jpRU = new JPanel();
         jpRD = new JPanel();
+        jpBottom = new JPanel();
+        jpBottom.setLayout(new BorderLayout());
 
         jspCenter = new JSplitPane();
         jspLeft = new JSplitPane();
         jspRight = new JSplitPane();
 
-        imgIM1 = new ImageIcon("./img/img_Button1.png");
-        imgIM2 = new ImageIcon("./img/img_Button2.png");
-        imgOC1 = new ImageIcon("./img/img_Button3.png");
-        imgOC2 = new ImageIcon("./img/img_Button4.png");
+        imgIM1 = new ImageIcon("./img/img_IM1.jpg");
+        imgIM2 = new ImageIcon("./img/img_IM2.jpg");
+        imgOC1 = new ImageIcon("./img/img_OC1.jpg");
+        imgOC2 = new ImageIcon("./img/img_OC2.jpg");
+        imgAlarm1 = new ImageIcon("./img/img_Alarm1.jpg");
+        imgAlarm2 = new ImageIcon("./img/img_Alarm2.jpg");
+        imgCal = new ImageIcon("./img/img_Cal.jpg");
+        imgIMopt1_1 = new ImageIcon("./img/img_IMopt1_1.jpg");
+        imgIMopt1_2 = new ImageIcon("./img/img_IMopt1_2.jpg");
+        imgIMopt2_1 = new ImageIcon("./img/img_IMopt2_1.jpg");
+        imgIMopt2_2 = new ImageIcon("./img/img_IMopt2_2.jpg");
 
 
         jpLU.setBackground(Color.WHITE);
@@ -113,15 +125,31 @@ public class Main extends JFrame{
         jspRight.setDividerSize(7);
 
         btnInventoryManagement = new JButton(imgIM1);
+        btnInventoryManagement.setBorder(BorderFactory.createEmptyBorder(5, 5, 5,5));
         btnOrderConsolidation = new JButton(imgOC1);
-        jlIMopt1 = new JLabel("    > 개별 등록");
-        jlIMopt1.setBorder(BorderFactory.createEmptyBorder(15, 0, 15, 0));
-        jlIMopt1.setFont(font1);
-        jlIMopt2 = new JLabel("    > 일괄 등록");
-        jlIMopt2.setFont(font1);
-        jlIMopt2.setBorder(BorderFactory.createEmptyBorder(15, 0, 15, 0));
-        jlIMopt1.setVisible(false);
-        jlIMopt2.setVisible(false);
+        btnOrderConsolidation.setBorder(BorderFactory.createEmptyBorder(5, 5, 5,5));
+        btnIMopt1 = new JButton(imgIMopt1_1);
+        btnIMopt1.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+//        jlIMopt1.setFont(font1);
+        btnIMopt1.setRolloverIcon(imgIMopt1_2); // 버튼에 마우스가 올라갈떄 이미지 변환
+        btnIMopt1.setBorderPainted(false); // 버튼 테두리 제거
+        btnIMopt1.setFocusPainted(false);
+        btnIMopt1.setContentAreaFilled(false);
+
+        btnIMopt2 = new JButton(imgIMopt2_1);
+        btnIMopt2.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+//        jlIMopt2.setFont(font1);
+        btnIMopt2.setRolloverIcon(imgIMopt2_2); // 버튼에 마우스가 올라갈떄 이미지 변환
+        btnIMopt2.setBorderPainted(false); // 버튼 테두리 제거
+        btnIMopt2.setFocusPainted(false);
+        btnIMopt2.setContentAreaFilled(false);
+
+        btnIMopt1.setVisible(false);
+        btnIMopt2.setVisible(false);
+        jlCalendar = new JLabel(imgCal);
+        jlCalendar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        jlCalendar.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
+
 
         // 재고 관리 버튼
         btnInventoryManagement.setRolloverIcon(imgIM2); // 버튼에 마우스가 올라갈떄 이미지 변환
@@ -133,19 +161,19 @@ public class Main extends JFrame{
         btnInventoryManagement.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (jlIMopt1.isVisible()) {
-                    jlIMopt1.setVisible(false);
-                    jlIMopt2.setVisible(false);
+                if (btnIMopt1.isVisible()) {
+                    btnIMopt1.setVisible(false);
+                    btnIMopt2.setVisible(false);
                 }
                 else {
-                    jlIMopt1.setVisible(true);
-                    jlIMopt2.setVisible(true);
+                    btnIMopt1.setVisible(true);
+                    btnIMopt2.setVisible(true);
                 }
             }
         });
 
         // 개별 등록 라벨
-        jlIMopt1.addMouseListener(new MouseAdapter() {
+        btnIMopt1.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e){
                 String opt1Title = new String("재고 관리(개별 등록)");
                 if (findTabByName(opt1Title, jtpMainTab) != -1) {
@@ -159,7 +187,7 @@ public class Main extends JFrame{
         });
 
         // 일괄 등록 라벨
-        jlIMopt2.addMouseListener(new MouseAdapter() {
+        btnIMopt2.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e){
                 String opt2Title = new String("재고 관리(일괄 등록)");
                 if (findTabByName(opt2Title, jtpMainTab) != -1) {
@@ -192,6 +220,14 @@ public class Main extends JFrame{
             }
         });
 
+        btnAlarm = new JButton(imgAlarm1);
+        btnAlarm.setRolloverIcon(imgAlarm2); // 버튼에 마우스가 올라갈떄 이미지 변환
+        btnAlarm.setPreferredSize(new Dimension(74, 29)); // 버튼 크기 지정
+        btnAlarm.setBorderPainted(false); // 버튼 테두리 제거
+        btnAlarm.setFocusPainted(false);
+        btnAlarm.setContentAreaFilled(false);
+        btnAlarm.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+
         jtpMainTab.addTab("tab1", new JPanel());
         jtpMainTab.addTab("tab2", new JPanel());
 
@@ -216,8 +252,8 @@ public class Main extends JFrame{
         jpLU.setLayout(new BoxLayout(jpLU,BoxLayout.Y_AXIS));
 
         jpLU.add(btnInventoryManagement);    // 재고 관리 버튼 추가
-        jpLU.add(jlIMopt1);
-        jpLU.add(jlIMopt2);
+        jpLU.add(btnIMopt1);
+        jpLU.add(btnIMopt2);
         jpLU.add(btnOrderConsolidation);
         
         // 우상단 패널
@@ -249,7 +285,14 @@ public class Main extends JFrame{
         jspRight.setRightComponent(jpRD);
         jspRight.setDividerLocation(sizeHeight/2);          // 오른쪽 jsp의 높이 설정
 
-        getContentPane().add(jspCenter);
+        jpMain.setLayout(new BorderLayout());
+        jpMain.add(jspCenter, BorderLayout.CENTER);
+        jpBottom.add(jlCalendar, BorderLayout.WEST);
+        jpBottom.add(Box.createHorizontalGlue());
+        jpBottom.add(btnAlarm, BorderLayout.EAST);
+        jpMain.add(jpBottom, BorderLayout.SOUTH);
+
+        getContentPane().add(jpMain);
     }
 
     // 탭 메뉴에 x버튼 추가하는 클래스(처음 탭은 x버튼 없음)
@@ -422,11 +465,9 @@ public class Main extends JFrame{
         }
     }
     // 탭 타이틀 이름을 찾아 인덱스를 반환하는 함수
-    public int findTabByName(String title, JTabbedPane tab)
-    {
+    public int findTabByName(String title, JTabbedPane tab) {
         int tabCount = tab.getTabCount();
-        for (int i=0; i < tabCount; i++)
-        {
+        for (int i=0; i < tabCount; i++) {
             String tabTitle = tab.getTitleAt(i);
             if (tabTitle.equals(title)) return i;
         }
@@ -435,8 +476,6 @@ public class Main extends JFrame{
 
     public static void main(String[] args) {
         new Main();
-
-
     }
 }
 
