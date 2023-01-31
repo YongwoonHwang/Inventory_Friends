070b563b786e56b60ba8a83810a1407b796ab480
ookkeeeee <107971563+ookkeeeee@users.noreply.github.com>
2023-02-01 06:49:44 +0900

    Update Database.java

--- LoginUi/Database.java
+++ LoginUi/Database.java
@@ -1,13 +1,23 @@
-package LoginUi;
-import java.sql.*;
+package com.localapp.localtest.LoginUi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class Database {
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
     Connection con = null;
     Statement stmt = null;
-    String url = "jdbc:mysql://localhost/solodb?useSSL=false&serverTimezone=Asia/Seoul";
+    String url = "jdbc:mysql://inventoryfriends.cxtfsxnxj3jt.ap-northeast-1.rds.amazonaws.com/kmj?characterEncoding=utf8&useUnicode=true&mysqlEncoding=utf8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul";
 
+    String user = "admin";
+    String passwd = "admin1470";
 
-    String user = "root";
-    String passwd = "db비밀번호";
 
     Database() {
         try {
@@ -26,15 +36,17 @@ public class Database {
         String pw = _p;
 
         try {
-            String checkingStr = "SELECT CONVERT(AES_DECRYPT(UNHEX(Pw),SHA2(\"abcabc\",256)) USING UTF8) DECPASS FROM userlogin WHERE UserID = '" + id + "'";
+            String checkingStr = "SELECT password FROM user WHERE userID = '" + id + "'";
             System.out.println(checkingStr);
             ResultSet result = stmt.executeQuery(checkingStr);
 
 
             int count = 0;
             while(result.next()) {
-//                System.out.println(result.getString("DECPASS"));
-                if(pw.equals(result.getString("DECPASS"))) {
+                System.out.println("db에서 가져온 password = "+ result.getString("password"));
+                System.out.println("입력한 패스워드 = "+ pw);
+
+                if(passwordEncoder.matches(_p,result.getString("password"))) {
 
                     flag = true;
                     System.out.println("로그인 성공");
@@ -47,7 +59,7 @@ public class Database {
             }
         } catch (Exception e) {
             flag =false;
-            System.out.println("로그인 실패 > " + e.toString());
+            System.out.println("로그인 실패 > " + e.toString() + passwordEncoder.matches(pw,"$2a$10$dSy8.JCNFZRwzQBlw/6eeenT.lcSILGJ/h2R7ewr46JEx5XxtV/d6") );
         }
         return flag;
     }
