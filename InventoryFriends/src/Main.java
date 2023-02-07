8e95a46a1da03726c21a76da5f8f0ea3355269d0
YongwoonHwang <107971619+YongwoonHwang@users.noreply.github.com>
2023-02-07 22:36:15 +0900

    Add files via upload

--- InventoryFriends/src/Main.java
+++ InventoryFriends/src/Main.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.sql.*;
 import javax.swing.*;
 import javax.swing.plaf.*;
 import javax.swing.table.*;
@@ -8,8 +10,7 @@ public class Main extends JFrame{
     JPanel jpMain, jpLU, jpLD, jpRU, jpRD, jpBottom, jpOrderConsolidation, jpOCSearch, jpOCTable;
     JMenuBar jmbMenuBar;
     JMenu jmFileMenu;
-    JButton btnSignOut, btnInventoryManagement, btnOrderConsolidation, btnAlarm, btnIMopt1, btnIMopt2,
-            btnSearch;
+    JButton btnSignOut, btnInventoryManagement, btnOrderConsolidation, btnAlarm, btnIMopt1, btnIMopt2, btnSearch;
     JSplitPane jspCenter, jspLeft, jspRight;
     JLabel jlUserName, jlCalendar;
     JTabbedPane jtpMainTab, jtpSubTab;
@@ -22,14 +23,21 @@ public class Main extends JFrame{
     Font font1, font2;
 
     //용운 변수 start //
-    JPanel TextFields1, TextFields2;
-    JButton btnSubmit1, btnSubmit2, btnSubmit3, btnSubmit4;
-    JLabel text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12, jlCalendar2, jlCalendar3;
-    HintTextField hint1, hint2, hint3, hint5, hint6;
-    JComboBox dropdown4;
-    ImageIcon imgSubmit, imgAttach;
-    JTextField  jTextField1, jTextField2, jTextField3, jTextField5, jTextField6,
-            jTextField7, jTextField8, jTextField9, jTextField10;
+    JPanel jpIndividualRegistration, jpBatchRegistration, jpInventoryStatus;
+    JButton btnSubmit1, btnSubmit2, btnSubmit3, btnSubmit4, btnCal1, btnCal2, btnCalc;
+    JLabel text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12,
+            jlCalendar2, jlCalendar3, jlCalc, jlfilechooser;
+    HintTextField htfCategory, htfCode, htfProductName, htfQuantity, htfProductLocation;
+    JComboBox jcbMarketChoose;
+    ImageIcon imgSubmit, imgAttach1, imgAttach2, imgCalc, imgCal1, imgCal2;
+    JTextField jTextField7, jTextField8, jTextField9, jTextField10;
+    JTable jtInventoryStatus;
+    Calculator JCalc;
+//    CalendarWindow regiCal1, regiCal2;
+    JFileChooser filechooser;
+    JFrame windowCalc;
+
+//    DefaultTableModel jdtStockStat;
     //용운 변수 end //
     private int sizeWidth = 1280;
     private int sizeHeight = 720;
@@ -43,6 +51,7 @@ public class Main extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
     private void createMenu() {
         jmbMenuBar = new JMenuBar(); // MenuBar 컴포넌트 생성
         jmFileMenu = new JMenu("File"); // "파일" 메뉴 컴포넌트 생성
@@ -164,117 +173,182 @@ public class Main extends JFrame{
         jlCalendar.setCursor(new Cursor(Cursor.HAND_CURSOR));
         jlCalendar.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
 
-        // 용운 code start //
+// 용운 code start //
 
         imgSubmit = new ImageIcon("./img/img_submit.jpg");
-        imgAttach = new ImageIcon("./img/img_attach.jpg");
+        imgAttach1 = new ImageIcon("./img/img_attach.jpg");
+        imgAttach2 = new ImageIcon("./img/img_attach.jpg");
+        imgCalc = new ImageIcon("./img/img_Calc.jpg");
+        imgCal1 = new ImageIcon("./img/img_Cal.jpg");
+        imgCal2 = new ImageIcon("./img/img_Cal.jpg");
 
-        TextFields1 = new JPanel();
-        TextFields2 = new JPanel();
-        String Markets[] = {"쿠팡", "네이버", "11번가", "위메프", "옥션", "G마켓", "티몬"};
+        jpIndividualRegistration = new JPanel();
+        jpIndividualRegistration.setLayout(null);
+        jpBatchRegistration = new JPanel();
+        jpBatchRegistration.setLayout(null);
+        jpInventoryStatus = new JPanel();
 
         jlCalendar2 = new JLabel(imgSubmit);
         jlCalendar2.setCursor(new Cursor(Cursor.HAND_CURSOR));
         jlCalendar2.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+        jpIndividualRegistration.add(jlCalendar2);
 
-        jlCalendar3 = new JLabel(imgAttach);
+        jlCalendar3 = new JLabel(imgAttach1);
         jlCalendar3.setCursor(new Cursor(Cursor.HAND_CURSOR));
         jlCalendar3.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
+        jpIndividualRegistration.add(jlCalendar3);
 
-        jTextField1 = new JTextField();
-        jTextField1.setBounds(140,15,700,25);
+        JCalc = new Calculator();
+        filechooser = new JFileChooser();
+        jlfilechooser = new JLabel();
 
-        jTextField2 = new JTextField();
-        jTextField2.setBounds(140,45,700,25);
 
-        jTextField3 = new JTextField();
-        jTextField3.setBounds(140,75,700,25);
+        //테이블 패널
+        String header2[] = {"CATEGORY", "CODE", "NAME", "QUANTITY", "MARKET", "LOCATION", "STOCKING_DATE", "EDA", "IMAGE"};
 
-        dropdown4 = new JComboBox(Markets);
-        dropdown4.setBounds(140, 105, 700, 25);
+        jtInventoryStatus = new JTable();
+        // 테이블 속성 오버라이드
+        DefaultTableModel model2 = new DefaultTableModel(header2,0){
+            @Override
+            public boolean isCellEditable(int row, int col){
+                return false;
+            }
+        };
+        jtInventoryStatus.setModel(model2);
+        resizeColumnWidth(jtInventoryStatus);
 
-        jTextField5 = new JTextField();
-        jTextField5.setBounds(140,135,700,25);
+        jpInventoryStatus.setLayout(new BorderLayout());
+        jpInventoryStatus.add(new JScrollPane(jtInventoryStatus), BorderLayout.CENTER);
 
-        jTextField6 = new JTextField();
-        jTextField6.setBounds(140,165,700,25);
+        jcbMarketChoose = new JComboBox();
+        jcbMarketChoose.addItem("");
+        jcbMarketChoose.addItem("쿠팡");
+        jcbMarketChoose.addItem("네이버");
+        jcbMarketChoose.addItem("11번가");
+        jcbMarketChoose.addItem("위메프");
+        jcbMarketChoose.addItem("옥션");
+        jcbMarketChoose.addItem("G마켓");
+        jcbMarketChoose.addItem("티몬");
+        jcbMarketChoose.setBounds(140, 135, 700, 25);
+        jcbMarketChoose.setBackground(Color.WHITE);
+        jcbMarketChoose.setFont(font1);
+        jpIndividualRegistration.add(jcbMarketChoose);
 
         jTextField7 = new JTextField();
-        jTextField7.setBounds(140,195,700,25);
+        jTextField7.setBounds(140,195,675,25);
+        jpIndividualRegistration.add(jTextField7);
 
         jTextField8 = new JTextField();
-        jTextField8.setBounds(140,225,700,25);
+        jTextField8.setBounds(140,225,675,25);
+        jpIndividualRegistration.add(jTextField8);
 
         jTextField9 = new JTextField();
         jTextField9.setBounds(140,255,200,25);
         jTextField9.setEditable(false);
+        jpIndividualRegistration.add(jTextField9);
 
         jTextField10 = new JTextField();
         jTextField10.setBounds(140,100,200,30);
         jTextField10.setEditable(false);
+        jpBatchRegistration.add(jTextField10);
 
-        text1 = new JLabel("코드 : ");
+        text1 = new JLabel("카테고리 : ");
         text1.setBounds(20,15,120,30);
+        jpIndividualRegistration.add(text1);
 
-        text2 = new JLabel("품명 : ");
+        text2 = new JLabel("코드 : ");
         text2.setBounds(20,45,120,30);
+        jpIndividualRegistration.add(text2);
 
-        text3 = new JLabel("수량 : ");
+        text3 = new JLabel("품명 : ");
         text3.setBounds(20,75,120,30);
+        jpIndividualRegistration.add(text3);
 
-        text4 = new JLabel("마켓 선택 : ");
+        text4 = new JLabel("수량 : ");
         text4.setBounds(20,105,120,30);
+        jpIndividualRegistration.add(text4);
 
-        text5 = new JLabel("카테고리 : ");
+        text5 = new JLabel("마켓 선택 : ");
         text5.setBounds(20,135,120,30);
+        jpIndividualRegistration.add(text5);
 
         text6 = new JLabel("재고 위치 : ");
         text6.setBounds(20,165,120,30);
+        jpIndividualRegistration.add(text6);
 
         text7 = new JLabel("최근 입고일 : ");
         text7.setBounds(20,195,120,30);
+        jpIndividualRegistration.add(text7);
 
         text8 = new JLabel("다음 입고 예정일 : ");
         text8.setBounds(20,225, 120, 30);
+        jpIndividualRegistration.add(text8);
 
         text9 = new JLabel("이미지 : ");
         text9.setBounds(20,255, 120, 30);
+        jpIndividualRegistration.add(text9);
 
         text10 = new JLabel("일괄 등록 : ");
         text10.setBounds(20,100, 120, 30);
+        jpBatchRegistration.add(text10);
 
         text11 = new JLabel("* excel 혹은 csv 파일만 지원합니다. 파일 업로드 전에 지원 양식을 다시 한 번 확인해주시기 바랍니다.");
         text11.setBounds(20,140, 570, 30);
+        jpBatchRegistration.add(text11);
 
         text12 = new JLabel("자세히 보기");
         text12.setBounds(600,140, 150, 30);
+        jpBatchRegistration.add(text12);
 
-        hint1 = new HintTextField("코드는 필수 입력 항목입니다. (ex. CH-Cover-A-01-black)");
-        hint1.setBounds(140,15,700,25);
+        htfCategory = new HintTextField("카테고리는 필수 입력 항목입니다. (ex. Chair)");
+        htfCategory.setBounds(140,15,700,25);
+        jpIndividualRegistration.add(htfCategory);
 
-        hint2 = new HintTextField("품명은 필수 입력 항목입니다. (ex. 의자 커버 원형 플로럴 검정)");
-        hint2.setBounds(140,45,700,25);
+        htfCode = new HintTextField("코드는 필수 입력 항목입니다. (ex. Cover-A-01-Black)");
+        htfCode.setBounds(140,45,700,25);
+        jpIndividualRegistration.add(htfCode);
 
-        hint3 = new HintTextField("수량은 필수 입력 항목입니다. (ex. 10EX)");
-        hint3.setBounds(140,75,700,25);
+        htfProductName = new HintTextField("품명은 필수 입력 항목입니다. (ex. 의자 커버 원형 플로럴 검정)");
+        htfProductName.setBounds(140,75,700,25);
+        jpIndividualRegistration.add(htfProductName);
 
-        hint5 = new HintTextField("ex.의자-커버");
-        hint5.setBounds(140,135,700,25);
+        htfQuantity = new HintTextField("수량은 필수 입력 항목입니다.(ex. 10EA");
+        htfQuantity.setBounds(140,105,678,25);
+        jpIndividualRegistration.add(htfQuantity);
 
-        hint6 = new HintTextField("ex.Rack-01-A-05");
-        hint6.setBounds(140,165,700,25);
+        htfProductLocation = new HintTextField("ex.Rack-01-A-05");
+        htfProductLocation.setBounds(140,165,700,25);
+        jpIndividualRegistration.add(htfProductLocation);
 
 
         btnSubmit1 = new JButton("파일첨부");
+//        btnSubmit1.setRolloverIcon(imgAttach2); // 버튼에 마우스가 올라갈떄 이미지 변환
+//        btnSubmit1.setBorderPainted(false); // 버튼 테두리 제거
+//        btnSubmit1.setFocusPainted(false);
+//        btnSubmit1.setContentAreaFilled(false);
+//        btnSubmit1.setPreferredSize(new Dimension(85, 25)); // 버튼 크기 지정
+
         btnSubmit1.setBounds(340, 255, 85, 25);
+//        windowCalc.add(jlfilechooser);
         btnSubmit1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //파일오픈 다이얼로그 를 띄움
+                int result = filechooser.showOpenDialog(windowCalc);
 
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    //선택한 파일의 경로 반환
+                    File selectedFile = filechooser.getSelectedFile();
+
+                    //경로 출력
+                    System.out.println(selectedFile);
+//                    jlfilechooser.setText("저장 경로:" +filechooser.getSelectedFile().toString()+"."+filechooser.getFileFilter().getDescription());
+                }
             }
         });
+        jpIndividualRegistration.add(btnSubmit1);
 
-        btnSubmit2 = new JButton("파일첨부");
+        btnSubmit2 = new JButton();
         btnSubmit2.setBounds(340, 100,90,30);
         btnSubmit2.addActionListener(new ActionListener() {
             @Override
@@ -282,15 +356,88 @@ public class Main extends JFrame{
 
             }
         });
+        jpBatchRegistration.add(btnSubmit2);
+
 
         btnSubmit3 = new JButton("등록");
         btnSubmit3.setBounds(900, 280, 75, 25);
         btnSubmit3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                System.out.println(e.getActionCommand());  //선택된 버튼의 테스트값 출력
+                Connection con = null;
+                PreparedStatement pstmt = null;
+                ResultSet result = null;
+                String sql = "INSERT INTO yongun7_Inventory_List (CATEGORY, CODE, PRODUCT_NAME, QUANTITY, MARKET, PRODUCT_LOCATION, STOCKING_DATE, EDA, IMAGE) " + "VALUES (?,?,?,?,?,?,?,?,?)";
+                String sql2 = "SELECT * FROM yongun7_Inventory_List ORDER BY 'ID' ASC LIMIT 1";
+
+                try{
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    con = DriverManager.getConnection("jdbc:mysql://inventoryfriends.cxtfsxnxj3jt.ap-northeast-1.rds.amazonaws.com:3306/","admin","admin1470");
+                    // Statement는 정적 SQL문을 실행하고 결과를 반환받기 위한 객체다.
+                    //Statement하나당 한개의 ResultSet 객체만을 열 수 있다.
+
+                    pstmt = con.prepareStatement(sql);
+                    pstmt.execute("USE Inventory_List"); // 사용할 DB를 선택한다.
+                    // executeQuery : 쿼리를 실행하고 결과를 ResultSet 객체로 반환한다.
+
+                    pstmt.setString(1, htfCategory.getText());
+                    pstmt.setString(2, htfCode.getText());
+                    pstmt.setString(3, htfProductName.getText());
+                    pstmt.setInt(4, Integer.parseInt(htfQuantity.getText()));
+                    pstmt.setString(5, (String) jcbMarketChoose.getSelectedItem());
+                    pstmt.setString(6, htfProductLocation.getText());
+                    pstmt.setString(7, jTextField7.getText());
+                    pstmt.setString(8, jTextField8.getText());
+                    pstmt.setString(9, jTextField9.getText());
+
+                    int cnt = pstmt.executeUpdate();
+                    System.out.println("SUCCESS");
+
+//                    if(jTextField1.getText().isEmpty()) return;
+//                    jTextField1.setText("");//비우기
+
+                } catch (ClassNotFoundException cnfe) {
+                    System.out.println("DB 드라이버 로딩 실패 :" + cnfe.toString());
+                } catch (SQLException sqle) {
+                    System.out.println("DB 접속실패 : " + sqle.toString());
+                }
+
+                try{
+                    pstmt = con.prepareStatement(sql2);
+                    pstmt.execute("USE Inventory_List"); // 사용할 DB를 선택한다.
+                    result = pstmt.executeQuery(); //리턴 받아와서 데이터를 사용할 객체 생성
+                    while (result.next()){
+                        model2.addRow(new Object[]{result.getString("CATEGORY"), result.getString("CODE"), result.getString("Product_NAME"),
+                                result.getString("QUANTITY"), result.getString("MARKET"), result.getString("Product_Location"), result.getString("STOCKING_DATE"), result.getString("EDA"), result.getString("IMAGE")});
+
+                    }
+//                    jdtStockStat = (DefaultTableModel)jtInventoryStatus.getModel();
+//                    jdtStockStat.addRow(new String[]{"",""});
+
+                }catch(Exception cnfe){
+                    System.out.println(cnfe.getMessage());
+                }finally {
+                    try{
+                        result.close();
+                        pstmt.close();
+                        con.close();
+                    } catch (Exception e2) {}
+                }
+
+                String STTitle = new String("재고 현황");
+                if (findTabByName(STTitle, jtpSubTab) != -1) {
+                    jtpSubTab.setSelectedIndex(findTabByName(STTitle, jtpSubTab));
+                } else {
+                    jtpSubTab.addTab(STTitle, jpInventoryStatus);
+                    jtpSubTab.setSelectedIndex(findTabByName(STTitle, jtpSubTab));
+                }
 
             }
         });
+        jpIndividualRegistration.add(btnSubmit3);
+
+
 
         btnSubmit4 = new JButton("등록");
         btnSubmit4.setBounds(900, 280, 75, 25);
@@ -300,45 +447,40 @@ public class Main extends JFrame{
 
             }
         });
+        jpBatchRegistration.add(btnSubmit4);
 
-        TextFields1.setLayout(null);
-        TextFields1.add(text1);
-        TextFields1.add(hint1);
-        TextFields1.add(jTextField1);
-        TextFields1.add(text2);
-        TextFields1.add(hint2);
-        TextFields1.add(jTextField2);
-        TextFields1.add(text3);
-        TextFields1.add(hint3);
-        TextFields1.add(jTextField3);
-        TextFields1.add(text4);
-        TextFields1.add(dropdown4);
-        TextFields1.add(text5);
-        TextFields1.add(hint5);
-        TextFields1.add(jTextField5);
-        TextFields1.add(text6);
-        TextFields1.add(hint6);
-        TextFields1.add(jTextField6);
-        TextFields1.add(text7);
-        TextFields1.add(jlCalendar2);
-        TextFields1.add(jTextField7);
-        TextFields1.add(text8);
-        TextFields1.add(jlCalendar3);
-        TextFields1.add(jTextField8);
-        TextFields1.add(text9);
-        TextFields1.add(jTextField9);
-        TextFields1.add(btnSubmit1);
-        TextFields1.add(btnSubmit3);
+        btnCal1 = new JButton(imgCal1);
+        btnCal1.setBounds(815, 195, 23, 23);
+        btnCal1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                regiCal1.setLocation(jlCalendar2.getLocationOnScreen().x+1, jlCalendar2.getLocationOnScreen().y-181);
+            }
+        });
+        jpIndividualRegistration.add(btnCal1);
 
-        TextFields2.setLayout(null);
-        TextFields2.add(text10);
-        TextFields2.add(jTextField10);
-        TextFields2.add(btnSubmit2);
-        TextFields2.add(text11);
-        TextFields2.add(text12);
-        TextFields2.add(btnSubmit4);
+        btnCal2 = new JButton(imgCal2);
+        btnCal2.setBounds(815, 225, 23, 23);
+        btnCal2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-        //용운 code end//
+            }
+        });
+        jpIndividualRegistration.add(btnCal2);
+
+        btnCalc = new JButton(imgCalc);
+        btnCalc.setBounds(818, 105, 20, 23);
+        btnCalc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                JCalc.setLocation(jlCalc.getLocationOnScreen().x+1, jlCalc.getLocationOnScreen().y-181);
+            }
+        });
+        jpIndividualRegistration.add(btnCalc);
+
+
+//용운 code end//
 
         // 재고 관리 버튼
         btnInventoryManagement.setRolloverIcon(imgIM2); // 버튼에 마우스가 올라갈떄 이미지 변환
@@ -368,7 +510,7 @@ public class Main extends JFrame{
                 if (findTabByName(opt1Title, jtpMainTab) != -1) {
                     jtpMainTab.setSelectedIndex(findTabByName(opt1Title, jtpMainTab));
                 } else {
-                    jtpMainTab.addTab(opt1Title, TextFields1);
+                    jtpMainTab.addTab(opt1Title, jpIndividualRegistration);
                     jtpMainTab.setSelectedIndex(findTabByName(opt1Title, jtpMainTab));
                 }
 
@@ -382,7 +524,7 @@ public class Main extends JFrame{
                 if (findTabByName(opt2Title, jtpMainTab) != -1) {
                     jtpMainTab.setSelectedIndex(findTabByName(opt2Title, jtpMainTab));
                 } else {
-                    jtpMainTab.addTab(opt2Title, TextFields2);
+                    jtpMainTab.addTab(opt2Title, jpBatchRegistration);
                     jtpMainTab.setSelectedIndex(findTabByName(opt2Title, jtpMainTab));
                 }
 
@@ -452,6 +594,8 @@ public class Main extends JFrame{
         };
         jtOrderCon.setModel(model);
         resizeColumnWidth(jtOrderCon);
+
+
         jtOrderCon.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
